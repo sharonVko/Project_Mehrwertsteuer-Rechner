d3afcd63e97a2a9f251069cec7063b0b676b7024
Supercoder <supercoder@AirvonSpercoder.fritz.box>
2024-10-06 22:01:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# _Mehrwertsteuer-Rechner_
---
+++ src/vat-calculator/calculator.ts
@@ -0,0 +1,38 @@
+console.log("hello");
+
+
+const netto2brutto = document.querySelector('#netto2brutto') as HTMLInputElement;
+const brutto2netto = document.querySelector('#brutto2netto') as HTMLInputElement;
+const percent19 = document.querySelector('#percent19') as HTMLInputElement;
+const percent7 = document.querySelector('#percent7') as  HTMLInputElement;
+const insertAmount = document.querySelector('#insertAmount') as HTMLInputElement
+const calculateBtn = document.querySelector('#calculateBtn') as HTMLElement;
+
+
+
+console.log(netto2brutto);
+console.log(brutto2netto);
+console.log(percent19);
+console.log(percent7);
+console.log(insertAmount);
+console.log(calculateBtn);
+
+
+
+if(calculateBtn){
+    calculateBtn.addEventListener('click', () => {
+        console.log("test");
+      
+        
+    })
+};
+
+ /*  console.log(insertAmount.value);
+         */
+
+
+
+
+/* function calculation(){
+    if(Number())
+} */
